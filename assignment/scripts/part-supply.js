d7e341c16c490aae4ace164bb4273980aa3c6a62
Aman Abdirazak <amanabdirizak@icloud.com>
2022-11-13 16:37:13 -0600

    finished assignment to-dos

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -16,10 +16,12 @@ console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+let removedItem = supplyChanges.pop();
+console.log('4. Removed item:', removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+let newItem = supplyChanges.push(25);
+console.log('5. Adding 25 to supplyChanges.', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -27,7 +29,12 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (let x=0;  x< supplyChanges.length;  x++ ) {
+ if (supplyChanges[x] > 0) { console.log(`added ${supplyChanges[x]} parts.`);
+} else if (supplyChanges[x] < 0) { console.log (`removed ${supplyChanges[x]} parts`);
+} else { console.log('No change.', supplyChanges[x]);
+}
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
